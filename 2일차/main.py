780694f3506da5724af08c9c4c6504f251799287
HHJina <jina199234@gmail.com>
2026-01-12 11:13:02 +0900

    reset update

---
+++ 2일차/main.py
@@ -0,0 +1,120 @@
+from fastapi import FastAPI, HTTPException, Query
+
+from app.models.users import UserModel
+from app.models.movies import MovieModel
+
+from app.schemas.users import UserCreateRequest, UserResponse, UserUpdateRequest
+from app.schemas.movies import CreateMovieRequest, MovieUpdateRequest
+
+from typing import Annotated
+
+app = FastAPI()
+
+# 서버 시작 시 더미 데이터 생성
+UserModel.create_dummy()
+MovieModel.create_dummy()
+
+
+@app.get("/")
+async def root() -> dict[str, str]:
+    return {"Hello": "World"}
+
+
+# 유저생성
+@app.post("/users")
+def create_user(request: UserCreateRequest):
+    user = UserModel.create(username=request.username, age=request.age, gender=request.gender.value)
+    return user
+
+# 영화생성
+@app.post("/movies")
+def create_movie(request: CreateMovieRequest):
+    movie = MovieModel.create(title=request.title, playtime=request.playtime, genre=request.genre)
+    return movie
+
+
+# 모든유저
+@app.get("/users")
+def get_all_users():
+    return UserModel.all()
+
+# 모든영화
+@app.get("/movies")
+def get_all_movies():
+    return MovieModel.all()
+
+
+# 유저조회(복수)
+@app.get("/users/search")
+async def search_users(query_params: Annotated[UserResponse, Query()]):
+    valid_query = {key: value for key, value in query_params.model_dump().items() if value is not None}
+    filtered_users = UserModel.filter(**valid_query)
+    if not filtered_users:
+        raise HTTPException(status_code=404)
+
+    return filtered_users
+
+
+# 유저조회(단건)
+@app.get("/users/{user_id}")
+def get_user_by_id(user_id: int):
+    user = UserModel.get(id=user_id)
+    if user is None:
+        raise HTTPException(status_code=404)
+
+    return user
+
+# 영화조회(단건)
+@app.get("/movies/{movie_id}")
+def get_movie_by_id(movie_id: int):
+    movie = MovieModel.get(id=movie_id)
+    if movie is None:
+        raise HTTPException(status_code=404)
+
+    return movie
+
+# 유저수정
+@app.patch("/users/{user_id}")
+def update_user(user_id: int, request: UserUpdateRequest):
+    user = UserModel.get(id=user_id)
+    if user is None:
+        raise HTTPException(status_code=404)
+    user.update(**request.model_dump())
+
+    return user
+
+# 영화수정
+@app.patch("/movies/{movie_id}")
+def update_movie(movie_id: int, request: MovieUpdateRequest):
+    movie = MovieModel.get(id=movie_id)
+    if movie is None:
+        raise HTTPException(status_code=404)
+    movie.update(**request.model_dump())
+
+    return movie
+
+# 유저삭제
+@app.delete("/users/{user_id}")
+def delete_user(user_id: int) -> dict[str, int]:
+    user = UserModel.get(id=user_id)
+    if user is None:
+        raise HTTPException(status_code=404)
+    user.delete()
+
+    return {"delete_id": user_id}
+
+# 영화삭제
+@app.delete("/movies/{movie_id}")
+def delete_movie(movie_id: int) -> dict[str, int]:
+    movie = MovieModel.get(id=movie_id)
+    if movie is None:
+        raise HTTPException(status_code=404)
+    movie.delete()
+
+    return {"delete_id": movie_id}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
---
+++ 3일차/app/schemas/users.py
@@ -0,0 +1,30 @@
+from enum import Enum
+
+from pydantic import BaseModel
+
+
+# 1. 성별 제한을 위한 Enum 클래스
+class GenderEnum(str, Enum):
+    male = "male"
+    female = "female"
+
+
+# 2. 유저 생성 요청 (POST /users)
+class UserCreateRequest(BaseModel):
+    username: str
+    age: int
+    gender: GenderEnum
+
+
+# 3. 유저 수정 요청 (PATCH /users/{id})
+class UserUpdateRequest(BaseModel):
+    username: str | None = None
+    age: int | None = None
+
+
+# 4. 유저 응답 결과 (GET /users 등)
+class UserResponse(BaseModel):
+    id: int | None = None
+    username: str | None = None
+    age: int | None = None
+    gender: GenderEnum | None = None
